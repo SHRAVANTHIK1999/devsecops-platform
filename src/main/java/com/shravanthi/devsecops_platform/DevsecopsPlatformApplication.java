b28a4c8cc5c563d8300daaef529ae50b015daac5
shravanthik1999 <shravanthik1999@example.com>
2026-09-15 17:52:21 +0200

    Initial Spring Boot application

---
+++ src/main/java/com/shravanthi/devsecops_platform/DevsecopsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.shravanthi.devsecops_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsecopsPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsecopsPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shravanthi/devsecops_platform/DevsecopsPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shravanthi.devsecops_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsecopsPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
