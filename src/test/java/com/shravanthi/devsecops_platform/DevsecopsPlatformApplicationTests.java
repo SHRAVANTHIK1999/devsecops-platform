a1b4d73790373176bf9cda4a8a2a9c4891166b0e
shravanthik1999 <shravanthik1999@example.com>
2026-09-16 08:52:59 +0200

    Add test coverage with JaCoCo

--- src/test/java/com/shravanthi/devsecops_platform/DevsecopsPlatformApplicationTests.java
+++ src/test/java/com/shravanthi/devsecops_platform/DevsecopsPlatformApplicationTests.java
@@ -1,13 +1,32 @@
 package com.shravanthi.devsecops_platform;
 
+import com.shravanthi.devsecops_platform.controller.HealthController;
+import com.shravanthi.devsecops_platform.controller.HelloController;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class DevsecopsPlatformApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void helloEndpointShouldReturnMessage() {
+        HelloController controller = new HelloController();
 
+        String response = controller.hello();
+
+        assertThat(response).isEqualTo("Hello from DevSecOps Platform!");
+    }
+
+    @Test
+    void healthEndpointShouldReturnUpStatus() {
+        HealthController controller = new HealthController();
+
+        Map<String, String> response = controller.health();
+
+        assertThat(response)
+                .containsEntry("status", "UP")
+                .containsEntry("application", "devsecops-platform");
+    }
 }
